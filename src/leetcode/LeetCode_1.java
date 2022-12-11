780bde84364c1af71b55122e14de55514e2b2eb2
Gravasgun <1143528540@qq.com>
2022-12-11 08:20:50 +0800

    1.两数之和

---
+++ src/leetcode/LeetCode_1.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+public class LeetCode_1 {
+    /**
+     * 1.两数之和
+     * 给定一个整数数组nums和一个整数目标值target，请你在该数组中找出和为目标值target的那两个整数，并返回它们的数组下标。
+     * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+     * 你可以按任意顺序返回答案。
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (target == nums[i] + nums[j]) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+}
