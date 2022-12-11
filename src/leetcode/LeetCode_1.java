150af04619fd5fb1074819e6ee5e64b440ef0f23
Gravasgun <1143528540@qq.com>
2022-12-11 10:48:50 +0800

    pull test

--- src/leetcode/LeetCode_1.java
+++ src/leetcode/LeetCode_1.java
@@ -11,7 +11,6 @@ public class LeetCode_1 {
      * @return
      */
     public int[] twoSum(int[] nums, int target) {
-        int i=0;
         int[] result = new int[2];
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = i + 1; j < nums.length; j++) {
