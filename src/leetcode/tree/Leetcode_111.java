880b8e616d6d29201b8ae1cb96c607383ba12465
Gravasgun <1143528540@qq.com>
2023-04-21 22:14:37 +0800

    方法二：深度优先搜索(递归)

--- src/leetcode/tree/Leetcode_111.java
+++ src/leetcode/tree/Leetcode_111.java
@@ -10,7 +10,13 @@ import java.util.Queue;
  * 说明：叶子节点是指没有子节点的节点。
  */
 public class Leetcode_111 {
-    public int minDepth(TreeNode root) {
+    /**
+     * 方法一：广度优先搜索(层序遍历)
+     *
+     * @param root
+     * @return
+     */
+    public int minDepthMethodOne(TreeNode root) {
         int result = 0; // 记录二叉树的最小深度
         Queue<TreeNode> queue = new LinkedList<>(); // 创建队列
         if (root == null) { // 如果根节点为空，返回深度0
@@ -38,5 +44,32 @@ public class Leetcode_111 {
             }
         }
         return result; // 返回最小深度
+    }
+
+    /**
+     * 方法二：深度优先搜索(递归)
+     *
+     * @param root
+     * @return
+     */
+    public int minDepthMethodTwo(TreeNode root) {
+        // 如果根节点为空，直接返回深度为0
+        if (root == null) {
+            return 0;
+        }
+        // 递归计算左子树的深度
+        int leftHeight = minDepthMethodTwo(root.left);
+        // 递归计算右子树的深度
+        int rightHeight = minDepthMethodTwo(root.right);
+        // 如果当前节点的左子树为空，则最小深度为右子树的深度+1
+        if (root.left == null) {
+            return 1 + rightHeight;
+        }
+        // 如果当前节点的右子树为空，则最小深度为左子树的深度+1
+        if (root.right == null) {
+            return 1 + leftHeight;
+        }
+        // 如果当前节点的左右子树都不为空，则最小深度为左右子树深度的最小值+1
+        return 1 + Math.min(leftHeight, rightHeight);
     }
 }
