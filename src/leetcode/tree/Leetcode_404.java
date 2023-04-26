8e8f61264d68d7d0147e06e09f126b8c792c6b69
graves <1143528540@qq.com>
2023-04-26 13:33:35 +0800

    修改代码逻辑

--- src/leetcode/tree/Leetcode_404.java
+++ src/leetcode/tree/Leetcode_404.java
@@ -15,22 +15,23 @@ public class Leetcode_404 {
         if (root == null) {
             return 0;
         }
+        // 定义一个变量temp表示当前节点的值，默认为0
+        int temp = 0;
+        // 如果当前节点有左孩子，且左孩子是叶子节点，将其值赋给midValue变量
+        if (root.left != null && root.left.left == null && root.left.right == null) {
+            temp = root.left.val;
+        }
         // 递归计算左子树中所有左叶子节点的和，存储在leftSum变量中
         int leftSum = sumOfLeftLeavesMethodOne(root.left);
         // 递归计算右子树中所有左叶子节点的和，存储在rightSum变量中
         int rightSum = sumOfLeftLeavesMethodOne(root.right);
-        // 定义一个变量midValue表示当前节点的值，默认为0
-        int midValue = 0;
-        // 如果当前节点有左孩子，且左孩子是叶子节点，将其值赋给midValue变量
-        if (root.left != null && root.left.left == null && root.left.right == null) {
-            midValue = root.left.val;
-        }
         // 返回所有左叶子节点的和，即midValue与leftSum、rightSum相加的结果
-        return midValue + leftSum + rightSum;
+        return temp + leftSum + rightSum;
     }
 
     /**
      * 方法二：迭代
+     *
      * @param root
      * @return
      */
