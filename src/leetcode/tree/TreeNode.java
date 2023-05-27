2cc40aeda96cdf01b86b65d180e75ed3e773f81a
graves <1143528540@qq.com>
2023-05-27 10:19:53 +0800

    968.监控二叉树

--- src/leetcode/tree/TreeNode.java
+++ src/leetcode/tree/TreeNode.java
@@ -2,8 +2,8 @@ package leetcode.tree;
 
 public class TreeNode {
     int val;
-    TreeNode left;
-    TreeNode right;
+    public TreeNode left;
+    public TreeNode right;
 
     TreeNode() {
     }
