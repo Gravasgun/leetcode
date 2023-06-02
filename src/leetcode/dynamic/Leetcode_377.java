8252a26aed8643ccf86c5f880ca1857c85cf2193
graves <1143528540@qq.com>
2023-06-02 13:31:14 +0800

    377.组合总和Ⅳ 更新注释

--- src/leetcode/dynamic/Leetcode_377.java
+++ src/leetcode/dynamic/Leetcode_377.java
@@ -4,10 +4,13 @@ package leetcode.dynamic;
  * 377.组合总和Ⅳ
  * 给你一个由 不同 整数组成的数组 nums ，和一个目标整数 target 。请你从 nums 中找出并返回总和为 target 的元素组合的个数。
  * 题目数据保证答案符合 32 位整数范围。
+ *
+ * 本题是求排列 而518.零钱兑换II 是求组合
  */
 public class Leetcode_377 {
     public int combinationSum4(int[] nums, int target) {
         // 创建一个长度为target + 1的dp数组，用于存储计算结果
+        // dp[i]: 凑成目标正整数为i的排列个数为dp[i]
         int[] result = new int[target + 1];
         // 将result[0]初始化为1，表示组成目标值为0的组合数量为1
         result[0] = 1;
