30fe50c31ea572de59d612b991fc29b3e7eb1430
graves <1143528540@qq.com>
2023-07-03 14:20:29 +0800

    更新注释

--- src/leetcode/dynamic/Leetcode_123.java
+++ src/leetcode/dynamic/Leetcode_123.java
@@ -2,18 +2,41 @@ package leetcode.dynamic;
 
 public class Leetcode_123 {
     public int maxProfit(int[] prices) {
+        // result[i][j] 表示在第i天进行第j次交易时的最大利润
         int[][] result = new int[prices.length][5];
+        // j=0 表示未进行任何交易
+        // j=1 表示第一次持有
+        // j=2 表示第一次不持有
+        // j=3 表示第二次持有
+        // j=4 表示第二次不持有
         result[0][0] = 0;
         result[0][1] = -prices[0];
         result[0][2] = 0;
         result[0][3] = -prices[0];
         result[0][4] = 0;
         for (int i = 1; i < result.length; i++) {
+            // 计算进行第一次持有时的最大利润
+            // 可能的情况有两种：
+            // 1. 前一天已经第一次持有，今天不变
+            // 2. 前一天未进行任何交易，今天买入，利润减去股票价格
             result[i][1] = Math.max(result[i - 1][1], result[i - 1][0] - prices[i]);
+            // 计算进行第一次不持有时的最大利润
+            // 可能的情况有两种：
+            // 1. 前一天已经第一次不持有，今天不变
+            // 2. 前一天已经第一次持有，今天卖出，利润增加股票价格
             result[i][2] = Math.max(result[i - 1][2], result[i - 1][1] + prices[i]);
+            // 计算进行第二次持有时的最大利润
+            // 可能的情况有两种：
+            // 1. 前一天已经第二次持有，今天不变
+            // 2. 前一天已经第一次不持有，今天买入，利润减去股票价格
             result[i][3] = Math.max(result[i - 1][3], result[i - 1][2] - prices[i]);
+            // 计算进行第二次不持有时的最大利润
+            // 可能的情况有两种：
+            // 1. 前一天已经第二次不持有，今天不变
+            // 2. 前一天已经第二次持有，今天卖出，利润增加股票价格
             result[i][4] = Math.max(result[i - 1][4], result[i - 1][3] + prices[i]);
         }
+        // 返回最后一天进行两次卖出操作时的最大利润
         return Math.max(result[result.length - 1][3], result[result.length - 1][4]);
     }
 }
--- src/leetcode/dynamic/Leetcode_188.java
+++ src/leetcode/dynamic/Leetcode_188.java
@@ -2,6 +2,13 @@ package leetcode.dynamic;
 
 public class Leetcode_188 {
     public int maxProfit(int k, int[] prices) {
+        // result[i][j] 表示在第i天进行第j次交易时的最大利润
+        // j=0 表示未进行任何交易
+        // j=1 表示进行了一次买入操作
+        // j=2 表示进行了一次卖出操作
+        // j=3 表示进行了两次买入操作
+        // j=4 表示进行了两次卖出操作
+        // 以此类推，直到 j=2k 表示进行了k次卖出操作
         int[][] result = new int[prices.length][2 * k + 1];
         for (int i = 0; i < result[0].length; i++) {
             if (i % 2 == 0) {
@@ -13,13 +20,23 @@ public class Leetcode_188 {
 
         for (int i = 1; i < result.length; i++) {
             for (int j = 1; j < result[0].length; j++) {
+                // 当 j 为偶数时，表示进行卖出操作(不持有)
+                // 可能的情况有两种：
+                // 1. 前一天已经进行了相同次数的卖出操作(不持有)，今天不变
+                // 2. 前一天进行了相同次数的买入操作，今天卖出，利润增加股票价格
                 if (j % 2 == 0) {
                     result[i][j] = Math.max(result[i - 1][j], result[i - 1][j - 1] + prices[i]);
                 } else {
+                    // 当 j 为奇数时，表示进行买入操作
+                    // 可能的情况有两种：
+                    // 1. 前一天已经进行了相同次数的买入操作，今天不变
+                    // 2. 前一天进行了相同次数的卖出操作，今天买入，利润减去股票价格
                     result[i][j] = Math.max(result[i - 1][j], result[i - 1][j - 1] - prices[i]);
                 }
             }
         }
+        // 返回最后一天进行k次卖出操作时的最大利润
+        // 最大利润要么是最后一天进行k次卖出操作的情况，要么是最后一天进行k-1次卖出操作的情况
         return Math.max(result[result.length - 1][result[0].length - 2], result[result.length - 1][result[0].length - 1]);
     }
 }
