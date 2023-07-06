ae63f78a8c92d05dff7dc67f34bb2a1ec77cc2f7
graves <1143528540@qq.com>
2023-07-06 09:53:03 +0800

    重刷 300

---
+++ src/leetcode/dynamic/Leetcode_300.java
@@ -0,0 +1,18 @@
+package leetcode.dynamic;
+
+import java.util.Arrays;
+
+public class Leetcode_300 {
+    public int lengthOfLIS(int[] nums) {
+        int[] result = new int[nums.length];
+        Arrays.fill(result, 1);
+        for (int i = 1; i < result.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    result[i] = Math.max(result[i], result[j] + 1);
+                }
+            }
+        }
+        return result[result.length - 1];
+    }
+}
