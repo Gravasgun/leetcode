f2d700b8324e3c926c877e3a0f5add75865ffbab
graves <1143528540@qq.com>
2023-07-06 10:08:20 +0800

    重刷 674 暴力法

--- src/leetcode/dynamic/Leetcode_674.java
+++ src/leetcode/dynamic/Leetcode_674.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
  * 都有 nums[i] < nums[i + 1] ，那么子序列 [nums[l], nums[l + 1], ..., nums[r - 1], nums[r]] 就是连续递增子序列。
  */
 public class Leetcode_674 {
-    public int findLengthOfLCIS(int[] nums) {
+    public int findLengthOfLCISMethodOne(int[] nums) {
         // 创建一个dp数组来记录以每个位置结尾的最长连续递增子序列的长度
         int[] result = new int[nums.length];
         // 将数组 result 初始化为1，因为每个元素本身都构成长度为1的连续递增子序列
@@ -23,5 +23,28 @@ public class Leetcode_674 {
         }
         // 返回最长连续递增子序列的长度，即 result 数组中的最大值
         return Arrays.stream(result).max().getAsInt();
+    }
+
+    /**
+     * 计算给定数组的最长连续递增子序列的长度
+     *
+     * @param nums 给定的数组
+     * @return 最长连续递增子序列的长度
+     */
+    public int findLengthOfLCISMethodTwo(int[] nums) {
+        int result = 1; // 最长连续递增子序列的初始长度为1
+        for (int i = 0; i < nums.length - 1; i++) {
+            int temp = 1; // 用于记录当前递增子序列的长度
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] > nums[i]) {
+                    temp += 1; // 当前元素大于前一个元素，递增子序列长度加1
+                    i++; // 移动指针到下一个元素
+                    result = Math.max(result, temp); // 更新最长连续递增子序列的长度
+                } else {
+                    break; // 当前元素小于等于前一个元素，不再满足递增条件，跳出内层循环
+                }
+            }
+        }
+        return result; // 返回最长连续递增子序列的长度
     }
 }
