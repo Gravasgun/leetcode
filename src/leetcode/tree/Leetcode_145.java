ec7c48455d59eba90564af81915499766d7c217a
graves <1143528540@qq.com>
2023-07-10 10:47:35 +0800

    重刷 145 迭代

--- src/leetcode/tree/Leetcode_145.java
+++ src/leetcode/tree/Leetcode_145.java
@@ -2,9 +2,10 @@ package leetcode.tree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Leetcode_145 {
-    public List<Integer> postorderTraversal(TreeNode root) {
+    public List<Integer> postorderTraversalMethodOne(TreeNode root) {
         List<Integer> list = new ArrayList<>();
         if (root == null) {
             return list;
@@ -20,5 +21,25 @@ public class Leetcode_145 {
         postOrder(temp.left, list);
         postOrder(temp.right, list);
         list.add(temp.val);
+    }
+
+    public List<Integer> preorderTraversalMethodTwo(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            list.add(node.val);
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        return list;
     }
 }
