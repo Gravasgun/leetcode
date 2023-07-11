cdf01c8f5c3b441830e3704a6dce79717e8b4d8a
graves <1143528540@qq.com>
2023-07-11 10:35:23 +0800

    重刷 104

---
+++ src/leetcode/tree/Leetcode_104.java
@@ -0,0 +1,12 @@
+package leetcode.tree;
+
+public class Leetcode_104 {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+}
