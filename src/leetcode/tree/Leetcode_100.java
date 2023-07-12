2781eb0746ce3c665c90b88e54fee2cba3aadf65
graves <1143528540@qq.com>
2023-07-12 10:58:42 +0800

    重刷 100

---
+++ src/leetcode/tree/Leetcode_100.java
@@ -0,0 +1,19 @@
+package leetcode.tree;
+
+public class Leetcode_100 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null && q != null) {
+            return false;
+        }
+        if (p != null && q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
