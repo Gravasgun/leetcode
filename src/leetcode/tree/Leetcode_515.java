f884e7060d934338f8b64de620c8392cfd221a5d
graves <1143528540@qq.com>
2023-07-12 10:03:11 +0800

    重刷 515

---
+++ src/leetcode/tree/Leetcode_515.java
@@ -0,0 +1,31 @@
+package leetcode.tree;
+
+import java.util.*;
+
+public class Leetcode_515 {
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            while (size-- > 0) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            Integer max = Collections.max(list);
+            result.add(max);
+        }
+        return result;
+    }
+}
