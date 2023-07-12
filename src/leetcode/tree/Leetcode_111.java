295193609e221b838e78e506b516e1dd7c237d69
graves <1143528540@qq.com>
2023-07-12 10:33:17 +0800

    重刷 111

---
+++ src/leetcode/tree/Leetcode_111.java
@@ -0,0 +1,18 @@
+package leetcode.tree;
+
+public class Leetcode_111 {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDepth = minDepth(root.left);
+        int rightDepth = minDepth(root.right);
+        if (root.left == null) {
+            return rightDepth + 1;
+        }
+        if (root.right == null) {
+            return leftDepth + 1;
+        }
+        return Math.min(leftDepth, rightDepth) + 1;
+    }
+}
