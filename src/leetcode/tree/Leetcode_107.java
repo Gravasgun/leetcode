ece810cbd0c1909418f39296f0f38b5058e3496e
graves <1143528540@qq.com>
2023-07-12 09:35:29 +0800

    重刷 107

---
+++ src/leetcode/tree/Leetcode_107.java
@@ -0,0 +1,31 @@
+package leetcode.tree;
+
+import java.util.*;
+
+public class Leetcode_107 {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            while (size-- > 0) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            result.add(list);
+        }
+        Collections.reverse(result);
+        return result;
+    }
+}
