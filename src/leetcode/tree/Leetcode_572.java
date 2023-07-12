80df6425e5172b1eaecea407b020da27683df58a
graves <1143528540@qq.com>
2023-07-12 11:03:51 +0800

    重刷 572

---
+++ src/leetcode/tree/Leetcode_572.java
@@ -0,0 +1,29 @@
+package leetcode.tree;
+
+public class Leetcode_572 {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (subRoot == null) {
+            return true;
+        }
+        if (root == null) {
+            return false;
+        }
+        return isSameTree(root, subRoot) || isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null && q != null) {
+            return false;
+        }
+        if (p != null && q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
