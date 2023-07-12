b9aa20034d59d9d47099bbfbbd984a5a01af047f
graves <1143528540@qq.com>
2023-07-12 10:40:36 +0800

    重刷 101

---
+++ src/leetcode/tree/Leetcode_101.java
@@ -0,0 +1,26 @@
+package leetcode.tree;
+
+public class Leetcode_101 {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isSymmetricHelper(root.left, root.right);
+    }
+
+    private boolean isSymmetricHelper(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left != null && right == null) {
+            return false;
+        }
+        if (left == null && right != null) {
+            return false;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return isSymmetricHelper(left.left, right.right) && isSymmetricHelper(left.right, right.left);
+    }
+}
