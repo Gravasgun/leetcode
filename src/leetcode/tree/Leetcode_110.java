354b27af9525a6af61fe36eda2d454c9a7434d82
graves <1143528540@qq.com>
2023-07-12 09:21:00 +0800

    重刷 110

---
+++ src/leetcode/tree/Leetcode_110.java
@@ -0,0 +1,24 @@
+package leetcode.tree;
+
+public class Leetcode_110 {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        int leftHeight = getHeight(root.left);
+        int rightHeight = getHeight(root.right);
+        if (Math.abs(rightHeight - leftHeight) > 1) {
+            return false;
+        }
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    private int getHeight(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int leftHeight = getHeight(node.left);
+        int rightHeight = getHeight(node.right);
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
