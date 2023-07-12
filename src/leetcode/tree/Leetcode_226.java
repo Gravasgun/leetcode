8386c0656f614b5eb096a08ab23a1aa69b9ee66a
graves <1143528540@qq.com>
2023-07-12 10:36:06 +0800

    重刷 226

---
+++ src/leetcode/tree/Leetcode_226.java
@@ -0,0 +1,15 @@
+package leetcode.tree;
+
+public class Leetcode_226 {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
