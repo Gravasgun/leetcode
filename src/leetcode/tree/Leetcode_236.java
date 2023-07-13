671588ec150f898deccf79cdf6bce5d3faa644a5
graves <1143528540@qq.com>
2023-07-13 14:04:35 +0800

    重刷 236

---
+++ src/leetcode/tree/Leetcode_236.java
@@ -0,0 +1,20 @@
+package leetcode.tree;
+
+public class Leetcode_236 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left == null && right == null) {
+            return null;
+        } else if (left == null && right != null) {
+            return right;
+        } else if (left != null && right == null) {
+            return left;
+        } else {
+            return root;
+        }
+    }
+}
