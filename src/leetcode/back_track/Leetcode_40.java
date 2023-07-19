eedac02ef15316e43c6f7b7d933bc84c96f79ca9
graves <1143528540@qq.com>
2023-07-19 10:22:34 +0800

    重刷 40

---
+++ src/leetcode/back_track/Leetcode_40.java
@@ -0,0 +1,37 @@
+package leetcode.back_track;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Leetcode_40 {
+    List<List<Integer>> result = new ArrayList<>();
+    List<Integer> paths = new ArrayList<>();
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        if (candidates == null || candidates.length == 0) {
+            return result;
+        }
+        Arrays.sort(candidates);
+        backTrack(candidates, target, 0);
+        return result;
+    }
+
+    private void backTrack(int[] candidates, int target, int startIndex) {
+        if (paths.stream().mapToInt(Integer::intValue).sum() == target) {
+            result.add(new ArrayList<>(paths));
+            return;
+        }
+        if (paths.stream().mapToInt(Integer::intValue).sum() > target) {
+            return;
+        }
+        for (int i = startIndex; i < candidates.length; i++) {
+            if (i > startIndex && candidates[i - 1] == candidates[i]) {
+                continue;
+            }
+            paths.add(candidates[i]);
+            backTrack(candidates, target, i + 1);
+            paths.remove(paths.size() - 1);
+        }
+    }
+}
