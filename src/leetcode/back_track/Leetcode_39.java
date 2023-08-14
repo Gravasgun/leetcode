df4cea64291fdf0f5593d9f6ce22dc27ab8db749
graves <1143528540@qq.com>
2023-08-14 16:03:38 +0800

    39 三刷

--- src/leetcode/back_track/Leetcode_39.java
+++ src/leetcode/back_track/Leetcode_39.java
@@ -6,29 +6,30 @@ import java.util.List;
 
 public class Leetcode_39 {
     List<List<Integer>> result = new ArrayList<>();
-    List<Integer> list = new ArrayList<>();
+    List<Integer> path = new ArrayList<>();
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         if (candidates == null || candidates.length == 0) {
             return result;
         }
-        Arrays.sort(candidates);
         backTrack(candidates, target, 0);
         return result;
     }
 
     private void backTrack(int[] candidates, int target, int startIndex) {
-        if (list.stream().mapToInt(Integer::intValue).sum() == target) {
-            result.add(new ArrayList<>(list));
+        int sum = path.stream().mapToInt(Integer::intValue).sum();
+        if (sum == target) {
+            result.add(new ArrayList<>(path));
             return;
         }
-        if (list.stream().mapToInt(Integer::intValue).sum() > target) {
+        if (sum > target) {
             return;
         }
+
         for (int i = startIndex; i < candidates.length; i++) {
-            list.add(candidates[i]);
+            path.add(candidates[i]);
             backTrack(candidates, target, i);
-            list.remove(list.size() - 1);
+            path.remove(path.size() - 1);
         }
     }
 }
