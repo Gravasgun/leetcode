905275064ee2b66a4ac71197b201e96ab18b6ed6
graves <1143528540@qq.com>
2023-08-14 09:27:38 +0800

    77 三刷

--- src/leetcode/back_track/Leetcode_77.java
+++ src/leetcode/back_track/Leetcode_77.java
@@ -1,24 +1,17 @@
 package leetcode.back_track;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 public class Leetcode_77 {
-    // 存储组合结果的列表
-    // 不定义这两个全局变量也是可以的，把这两个变量放进递归函数的参数里，但函数里参数太多影响可读性，所以定义全局变量
     List<List<Integer>> result = new ArrayList<>();
-    // 当前正在生成的组合路径
-    List<Integer> path = new LinkedList<>();
+    List<Integer> path = new ArrayList<>();
 
     public List<List<Integer>> combine(int n, int k) {
-        // 调用组合辅助函数生成所有组合
         backTrack(n, k, 1);
-        // 返回最终的组合结果
         return result;
     }
 
-    // 回溯函数
     private void backTrack(int n, int k, int startIndex) {
         if (path.size() == k) {
             result.add(new ArrayList<>(path));
