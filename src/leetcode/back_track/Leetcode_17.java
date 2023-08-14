1c0c6aaeca78f034f8e6e9e2cb02db4c92d5775d
graves <1143528540@qq.com>
2023-08-14 15:35:24 +0800

    17 三刷

--- src/leetcode/back_track/Leetcode_17.java
+++ src/leetcode/back_track/Leetcode_17.java
@@ -4,28 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Leetcode_17 {
-    List<String> result = new ArrayList<>(); // 存储最终的结果列表
-    StringBuilder sb = new StringBuilder(); // 辅助构建字符串
+    List<String> result = new ArrayList<>();
+    StringBuilder sb = new StringBuilder();
 
     public List<String> letterCombinations(String digits) {
         if (digits == null || digits.length() == 0) {
-            return result; // 若输入为空或长度为0，则直接返回结果列表
+            return result;
         }
-        // 数字与对应字母的映射关系
-        String[] numString = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        backTrack(digits, numString, 0); // 进行回溯算法
-        return result; // 返回结果列表
+        String[] strings = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        backTrack(digits, strings, 0);
+        return result;
     }
 
-    private void backTrack(String digits, String[] numString, int index) {
+    private void backTrack(String digits, String[] strings, int index) {
         if (index == digits.length()) {
             result.add(sb.toString());
             return;
         }
-        String str = numString[Integer.parseInt(String.valueOf(digits.charAt(index)))];
-        for (int i = 0; i < str.length(); i++) {
-            sb.append(str.charAt(i));
-            backTrack(digits, numString, index + 1);
+        String s = strings[Integer.parseInt(String.valueOf(digits.charAt(index)))];
+        for (int i = 0; i < s.length(); i++) {
+            sb.append(s.charAt(i));
+            backTrack(digits, strings, index + 1);
             sb.deleteCharAt(sb.length() - 1);
         }
     }
