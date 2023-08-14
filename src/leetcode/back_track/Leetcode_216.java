b842db3497f7d9cd4a1e1f5441a695503acf6c7c
graves <1143528540@qq.com>
2023-08-14 09:50:35 +0800

    216 三刷

--- src/leetcode/back_track/Leetcode_216.java
+++ src/leetcode/back_track/Leetcode_216.java
@@ -1,26 +1,25 @@
 package leetcode.back_track;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 public class Leetcode_216 {
     List<List<Integer>> result = new ArrayList<>();
-    List<Integer> path = new LinkedList<>();
+    List<Integer> path = new ArrayList<>();
 
     public List<List<Integer>> combinationSum3(int k, int n) {
-        combination(k, n, 1);
+        backTrack(k, n, 1);
         return result;
     }
 
-    private void combination(int k, int n, int startIndex) {
+    private void backTrack(int k, int n, int startIndex) {
         if (path.size() == k && path.stream().mapToInt(Integer::intValue).sum() == n) {
             result.add(new ArrayList<>(path));
             return;
         }
         for (int i = startIndex; i <= 9; i++) {
             path.add(i);
-            combination(k, n, i + 1);
+            backTrack(k, n, i + 1);
             path.remove(path.size() - 1);
         }
     }
