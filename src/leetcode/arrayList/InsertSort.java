0dd3ee5513b3536f7d4a056054509676b472c154
graves <1143528540@qq.com>
2023-10-08 10:40:44 +0800

    插入排序

---
+++ src/leetcode/arrayList/InsertSort.java
@@ -0,0 +1,17 @@
+package leetcode.arrayList;
+
+public class InsertSort {
+    public void sort(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = i - 1; j >= 0 && nums[j] > nums[j + 1]; j--) {
+                swap(nums, j, j + 1);
+            }
+        }
+    }
+
+    private void swap(int[] nums, int index1, int index2) {
+        int temp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = temp;
+    }
+}
