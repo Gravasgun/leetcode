67521d16e1be06385d882faebb473a4561eecfc4
graves <1143528540@qq.com>
2023-10-08 11:16:11 +0800

    重刷35

---
+++ src/leetcode/arrayList/Leetcode_35.java
@@ -0,0 +1,19 @@
+package leetcode.arrayList;
+
+public class Leetcode_35 {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int middle = (left + right) / 2;
+            if (target > nums[middle]) {
+                left = middle + 1;
+            } else if (target < nums[middle]) {
+                right = middle - 1;
+            } else {
+                return middle;
+            }
+        }
+        return left;
+    }
+}
